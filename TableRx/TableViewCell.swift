18a47a8a1bee3ba91a144c8227faf9b819efcc5c
Md Sazzad Islam <sazzad@macvm>
2017-02-13 09:41:28 +0600

    table append prepend done

--- TableRx/TableViewCell.swift
+++ TableRx/TableViewCell.swift
@@ -10,17 +10,8 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-	@IBOutlet weak var label2: UILabel!
 	@IBOutlet weak var label1: UILabel!
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+	@IBOutlet weak var label2: UILabel!
+	
 
 }
--- TableRx/ViewController.swift
+++ TableRx/ViewController.swift
@@ -13,68 +13,82 @@ import RxCocoa
 struct Person {
 	let name : String
 	let age : Int
-	
 }
+
 class ViewController: UIViewController {
 	
 	let personArray = [
-		
 		Person(name: "name1", age: 11),
 		Person(name: "name2", age: 12),
-		Person(name: "name3", age: 13),
-		Person(name: "name4", age: 14),
-		Person(name: "name5", age: 15),
-		Person(name: "name6", age: 16),
-		Person(name: "name7", age: 17),
-		Person(name: "name8", age: 18),
-		Person(name: "name9", age: 19),
-		Person(name: "name10", age: 20),
-		Person(name: "name11", age: 21),
-		Person(name: "name12", age: 22),
-		Person(name: "name13", age: 22),
-		Person(name: "name14", age: 23),
-		Person(name: "name15", age: 24),
-		Person(name: "name16", age: 25),
-		Person(name: "name17", age: 26),
-		Person(name: "name18", age: 27)
-		
+		Person(name: "name3", age: 13)
 	]
-	
-	let disposeBag = DisposeBag()
 
+	let disposeBag = DisposeBag()
+	var items : Observable<[Person]>!
+
+	@IBAction func bb(_ sender: UIButton) {
+		let newPersonArray = [
+			Person(name: "name -3", age: 11),
+			Person(name: "name -4", age: 12)
+		]
+		prependData(dataToPrepend: newPersonArray)
+	}
+	
+	
+	
+	@IBAction func appendData(_ sender: UIButton) {
+		let newPersonArray = [
+			Person(name: "name 6", age: 11),
+			Person(name: "name 7", age: 12)
+		]
+		appendData(dataToAppend : newPersonArray)
+	}
+	
+	
 	@IBOutlet weak var mytable: UITableView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		let objArray : Observable<[Person]> = Observable.just(personArray)
-
-		objArray.bindTo(mytable.rx.items(cellIdentifier: "cell")) {
-			_ , person, cell in
+		items = Observable.just(personArray)
+		bindData()
+	}
+	
+	private func bindData() {
+		mytable.dataSource = nil
+		items.bindTo(mytable.rx.items(cellIdentifier: "cell")) { (row, person, cell) in
 			if let cellToUse = cell as? TableViewCell {
 				cellToUse.label1.text = person.name
 				cellToUse.label2.text = "\(person.age)"
 			}
 		}.addDisposableTo(disposeBag)
-		
-		
-		mytable.rx.modelSelected(Person.self).subscribe(onNext: {
-				person in
-			print(person.name)
-		}).addDisposableTo(disposeBag)
-		
-		
-		mytable.rx.itemSelected.subscribe(onNext : {
-			[weak self] indexPath in
-			if let cell = self?.mytable.cellForRow(at: indexPath) as? TableViewCell {
-				cell.label1.text = "new value"
-				
-				
-			}
-		}).addDisposableTo(disposeBag)
-		
 	}
-
-
-
+	
+	private func prependData(dataToPrepend : [Person]) {
+		let newObserver = Observable.just(dataToPrepend)
+		items = Observable.combineLatest(items, newObserver) {
+			$1+$0
+		}
+		bindData()
+	}
+	
+	private func appendData(dataToAppend : [Person]) {
+		let newObserver = Observable.just(dataToAppend)
+		items = Observable.combineLatest(items, newObserver) {
+			$0+$1
+		}
+		bindData()
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
